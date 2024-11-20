cd02d914b59678badfd830dc231ee3238f3f21f9
Marshall-Bits <kz_@hotmail.es>
2024-11-20 12:58:15 +0100

    add p

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <h1>If you see this, it means that the App component is working fine!</h1>
+      <p>
+        Check the console to see the output of the <code>console.log</code>
+      </p>
     </>
   );
 }
