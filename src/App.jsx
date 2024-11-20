99bae485149fc17def74c64486c3df2c20419fc2
Marshall-Bits <kz_@hotmail.es>
2024-11-20 12:50:58 +0100

    add h1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <h1>If you see this, it means that the App component is working fine!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
