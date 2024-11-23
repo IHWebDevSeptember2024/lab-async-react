68759688a2f82cdb594af6a86feae0258cd5446f
Roxana Ferramola <ferramolafvm@gmail.com>
2024-11-23 13:41:09 +0100

    working in iteration 4

--- src/api.js
+++ src/api.js
@@ -1,9 +1,29 @@
-function fetchData(){
-    fetch('https://swapi.dev/api/')
-  .then((response) => response.json())
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
+/*function fetchData() {
+  fetch("https://swapi.dev/api/")
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 }
+
+export default fetchData; */
+
+//Renderizado con async/await
+async function fetchData() {
+  try {
+    const response = await fetch("https://swapi.dev/api/");
+
+    if (response.status === 404) {
+      throw new Error("Not Found");
+    }
+
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData();
 
 export default fetchData;
