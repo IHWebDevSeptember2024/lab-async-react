84737bc76e170e73de508682e967082bb2c4f9ae
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-11-24 18:38:16 +0100

    Iteration 4 solved

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
-const fetchData = (apiUrl) => {
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch("Error loading the data");
-};
+async function fetchData(apiUrl) {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch {
+    console.log("Error loading the data");
+  }
+}
 
 export default fetchData;
