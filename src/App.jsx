58f349a2dd59c723b7e9e19c5c1595340ba33687
Tomás Martín <tmartindsgn@gmail.com>
2024-12-08 19:19:54 +0100

    lab donne

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
-import loadData from "./async-example";
 import fetchData from "./api";
 
 function App() {
+  async function getData() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   useEffect(() => {
-    fetchData().then((data) => console.log(data))
-      .catch((error) => console.error(error));
+    getData();
   }, []);
 
-  return <div className="App">check the console</div>;
+  return <div className="App">Fetching data with async/await...</div>;
 }
 
 export default App;
--- src/api.jsx
+++ src/api.jsx
@@ -1,11 +1,16 @@
-function fetchData(){
-
-    return fetch("https://pokeapi.co/api/v2/pokemon")
-        .then((response) => response.json())
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+async function fetchData() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
-    export default fetchData;
-  
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+export default fetchData;
